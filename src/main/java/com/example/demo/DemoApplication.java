e1c632428c378fbd388ee25bb3dd6db0ba671b9b
ARNAVANILPANDEY <arnavanilpandey2103@gmail.com>
2022-06-15 01:22:08 +0530

    Repo Creation

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+    @RequestMapping("home")
+    public String home()
+    {
+        System.out.println("Hello");
+        return "home.html";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
