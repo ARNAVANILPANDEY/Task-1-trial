2b8831eff0970053f27238ade9e79d0b661f67be
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2022-06-21 15:13:47 +0530

    read json

--- src/main/resources/static/script1.js
+++ src/main/resources/static/script1.js
@@ -7,6 +7,7 @@ function myFunction() {
      var Disbursement_Amount=$("#Disbursement Amount").val();
      var DisbursementDates=$("#fDisbursementDates").val();
      var tenure=$("#tenure").val();
+     var rate=$("#rate").val();
      var conversion=$("#conversion").val();
      var amount=$("#amount").val();
      var dates=$("#dates").val();
@@ -14,4 +15,20 @@ function myFunction() {
      var installment_formula=$("#installment_formula").val();
      var installment_freq=$("#installment_freq").val();
     
+
+     let text =[
+        {
+         "Installment No.": "1",
+         "Stage Number": "1",
+         "Installment Due Date": "10 Jul 22",
+         "Installment Amount": "4754.420694549732",
+         "Interest Rate": rate,
+         "Component 1 (Principal)": "3758.256310988088",
+         "Component 2 (Interest)": "996.1643835616438",
+         "Opening Balance": "101000.0",
+         "Closing Balance": "97241.74368901191"
+        },]
+        const obj = JSON.parse(JSON.stringify(text));
+        var fs = require('fs');
+fs.writeFile("thing.json", dictstring);
 }
