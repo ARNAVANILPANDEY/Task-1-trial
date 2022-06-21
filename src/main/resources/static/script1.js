0c77635545ed6bd8634e7fb39c0fa283b263d2e3
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2022-06-21 13:19:57 +0530

    Dynamic UI Table

---
+++ src/main/resources/static/script1.js
@@ -0,0 +1,17 @@
+function myFunction() {
+     var finance=$("#finance").val();
+     var repay_money=$("#repay_money").val();
+     var amount=$("#amount").val();
+     var Account_Open=$("#Account Open").val();
+     var Segment_Number=$("#Segment Number").val();
+     var Disbursement_Amount=$("#Disbursement Amount").val();
+     var DisbursementDates=$("#fDisbursementDates").val();
+     var tenure=$("#tenure").val();
+     var conversion=$("#conversion").val();
+     var amount=$("#amount").val();
+     var dates=$("#dates").val();
+     var repay_money=$("#repay_money").val();
+     var installment_formula=$("#installment_formula").val();
+     var installment_freq=$("#installment_freq").val();
+    
+}
